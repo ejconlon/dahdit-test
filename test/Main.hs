b409b007f95d40f65725947ddf4113a5b784d934
Eric Conlon <ejconlon@gmail.com>
2023-06-08 09:52:42 -0700

    etc

--- test/Main.hs
+++ test/Main.hs
@@ -7,15 +7,16 @@ module Main
 where
 
 import Control.Monad (join)
-import Dahdit (Binary, StaticByteSized, ViaGeneric (..), ViaStaticGeneric (..), Word16LE)
+import Dahdit (Binary, ByteCount (..), GetError, StaticByteSized (..), ViaGeneric (..), ViaStaticGeneric (..), Word16LE, decodeEnd, encode)
+import Data.ByteString (ByteString)
+import Data.ByteString qualified as BS
 import Data.Proxy (Proxy (..))
 import Data.Word (Word8)
 import GHC.Generics (Generic)
 import Test.Dahdit.Arb (Arb (..), ArbGeneric (..), DahditIdx)
-import Test.Dahdit.Tasty (FileExpect (..), UnitExpect (..), dahditMain, fileRT, genRT, staticRT, testRT, unitRT)
+import Test.Daytripper (Expect, MonadExpect (..), daytripperMain, expectDuring, mkExpect, mkFileRT, mkPropRT, mkUnitRT, testRT)
 import Test.Falsify.Generator (Gen)
-import Test.Tasty (TestTree, testGroup)
-import Test.Tasty.HUnit (testCase, (@?=))
+import Test.Tasty (testGroup)
 
 data P
 
@@ -37,28 +38,38 @@ data StaFoo = StaFoo !Word8 !Word16LE
   deriving (StaticByteSized, Binary) via (ViaStaticGeneric StaFoo)
   deriving (Arb I) via (ArbGeneric I StaFoo)
 
-testDummy :: TestTree
-testDummy = testCase "dummy" $ do
-  let actual = (1 + 1) :: Int
-      expected = 2 :: Int
-  actual @?= expected
+expectDD :: (MonadExpect m, Binary a, Eq a, Show a) => Expect m a ByteString (Either GetError a)
+expectDD = mkExpect enc dec
+ where
+  enc = expectLiftIO . encode
+  dec = expectLiftIO . fmap fst . decodeEnd
+
+expectBytes :: MonadExpect m => [Word8] -> Expect m a ByteString c -> Expect m a ByteString c
+expectBytes ws = expectDuring (\_ bs -> expectAssertEq bs (BS.pack ws))
+
+proxyBefore :: Expect m a b c -> Proxy a
+proxyBefore _ = Proxy
+
+expectStatic :: (MonadExpect m, StaticByteSized a) => Expect m a ByteString c -> Expect m a ByteString c
+expectStatic ex =
+  let len = unByteCount (staticByteSize (proxyBefore ex))
+  in  expectDuring (\_ bs -> expectAssertEq (BS.length bs) len) ex
 
 main :: IO ()
 main =
-  dahditMain
-    $ testGroup
-      "DahditTest"
-    $ testRT
-      <$> join
-        [
-          [ genRT "DynFoo prop" (arbI (Proxy @DynFoo))
-          , unitRT "DynFoo unit" (DynFoo 1 2) (UnitExpectBytes [1, 2, 0])
-          , fileRT "DynFoo file" "testdata/dynfoo.bin" (FileExpectValue (DynFoo 1 2))
+  daytripperMain $
+    testGroup "DahditTest" $
+      testRT
+        <$> join
+          [
+            [ mkPropRT "DynFoo prop" expectDD (arbI (Proxy @DynFoo))
+            , mkUnitRT "DynFoo unit" (expectBytes [1, 2, 0] expectDD) (DynFoo 1 2)
+            , mkFileRT "DynFoo file" expectDD "testdata/dynfoo.bin" (DynFoo 1 2)
+            ]
+            --     , let p = Proxy @StaFoo
+            --       in  staticRT p
+            --             <$> [ genRT "StaFoo prop" (arbI p)
+            --                 , unitRT "StaFoo unit" (StaFoo 3 4) (UnitExpectText "\ETX\EOT\NUL")
+            --                 , fileRT "DynFoo file" "testdata/stafoo.bin" (FileExpectValue (StaFoo 1 2))
+            --                 ]
           ]
-        , let p = Proxy @StaFoo
-          in  staticRT p
-                <$> [ genRT "StaFoo prop" (arbI p)
-                    , unitRT "StaFoo unit" (StaFoo 3 4) (UnitExpectText "\ETX\EOT\NUL")
-                    , fileRT "DynFoo file" "testdata/stafoo.bin" (FileExpectValue (StaFoo 1 2))
-                    ]
-        ]
